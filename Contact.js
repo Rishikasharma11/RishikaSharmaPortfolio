e8ffbcb6509dc84b01ebd352380c6f376b103ee0
Rishika Sharma <rishikaosharma@gmail.com>
2024-06-20 16:45:50 +0530

    Update Contact.js

--- Contact.js
+++ Contact.js
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Link } from "react-router-dom";
+import emailjs from '@emailjs/browser';
+import { ToastContainer, Flip, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Contact()  {
+   // --------------------------------------------subscribe to our newsletter--------------------------------
+   const form = useRef();
+
+   const sendEmail = (e) => {
+     e.preventDefault();
+ 
+     emailjs
+       .sendForm(
+         'service_2shjaqt', 
+         'template_5bfq8ud', 
+         form.current, {
+         publicKey: 'KapdrjZ67SvjEm1Jl',
+       })
+       .then(
+         () => {
+           toast.success('Message Sent', {
+             position: "top-center",
+             autoClose: 4000,
+             hideProgressBar: false,
+             closeOnClick: true,
+             pauseOnHover: true,
+             draggable: true,
+             progress: undefined,
+             theme: "colored",
+             transition: Flip,
+             });
+         },
+         (error) => {
+           console.log('FAILED...', error.text);
+         },
+       );
+   };
   return (
     <>
     <div className='md:px-46 lg:px-60 xl:px-80 pt-28 md:pt-40 text-center bg-[#e6dace] h-screen mx-0 max-w-full'>
@@ -25,15 +60,41 @@ function Contact()  {
       </div>
 
       <div className='flex items-center justify-center'>
-          <form className="max-w-2xl py-4 px-4 lg:py-12 text-center mx-auto space-y-4">
-                    <input type='text' placeholder='Name'
-                        className="w-full text-gray-800 rounded-md py-2.5 px-4 border text-sm" />
-                    <input type='email' placeholder='Email'
-                        className="w-full text-gray-800 rounded-md py-2.5 px-4 border text-sm" />
-                    <textarea placeholder='Message' rows="6"
-                        className="w-full text-gray-800 rounded-md px-4 border text-sm pt-2.5"></textarea>
-                    <button type='button'
-                        className="bg-blue-700 border-2 sm:text-lg border-blue-700 text-white font-semibold px-3 sm:px-5 py-1 rounded-full">Send</button>
+          <form  ref={form} onSubmit={sendEmail}
+          className="max-w-2xl py-4 px-4 lg:py-12 text-center mx-auto space-y-4">
+                    <input 
+                      type='text' 
+                      placeholder='Name' 
+                      name="user_name" 
+                      required 
+                      className="w-full text-gray-800 rounded-md py-2.5 px-4 border text-sm" />
+                    <input 
+                      type='email' 
+                      placeholder='Email'
+                      name="user_email" 
+                      className="w-full text-gray-800 rounded-md py-2.5 px-4 border text-sm" />
+                    <textarea 
+                      placeholder='Message' 
+                      rows="6"
+                      name="user_message" 
+                      className="w-full text-gray-800 rounded-md px-4 border text-sm pt-2.5"></textarea>
+                    <button 
+                      type='submit'
+                      value="Send"
+                     className="bg-blue-700 border-2 sm:text-lg border-blue-700 text-white font-semibold px-3 sm:px-5 py-1 rounded-full">Send</button>
+                     <ToastContainer
+                      position="top-center"
+                      autoClose={4000}
+                      hideProgressBar={false}
+                      newestOnTop={false}
+                      closeOnClick
+                      rtl={false}
+                      pauseOnFocusLoss
+                      draggable
+                      pauseOnHover
+                      theme="colored"
+                      transition= {Flip}
+                      />
           </form>
       </div>
    
